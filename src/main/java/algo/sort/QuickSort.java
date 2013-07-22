29f303156bb563ac54708bd6b4fa728c381cfcf8
Artur Khalikov <akhalikoff@gmail.com>
2013-07-22 05:24:29 +0400

    QuickSort with pivot of first element

--- src/main/java/algo/sort/QuickSort.java
+++ src/main/java/algo/sort/QuickSort.java
@@ -1,53 +1,60 @@
 package algo.sort;
 
+import util.FileUtils;
+
+import java.io.File;
+
 /**
  * Quick sort algorithm implementation
  * @author: Artur Khalikov
  */
 public class QuickSort {
 
+    public static int comparisons;
+
     public static void sort(int[] a) {
         if (a == null)
             throw new IllegalArgumentException("Input array is null");
-        if (a.length <= 1)
-            return;
-        sort(a, 0, a.length);
+        if (a.length > 1) {
+            comparisons = 0;
+            sort(a, 0, a.length);
+        }
     }
 
     private static void sort(int[] a, int lo, int hi) {
-        if (hi - lo <= 1)
-            return;
-        int j = partition(a, lo, hi);
-        sort(a, lo, j);
-        sort(a, j, hi);
+        if (hi - lo > 1) {
+            comparisons += hi - lo - 1;
+            int j = partition(a, lo, hi);
+            sort(a, lo, j-1);
+            sort(a, j, hi);
+        }
     }
 
     private static int partition(int[]a, int lo, int hi) {
-        int p = findPivot(a, lo);
-        int pivot = a[p];
+        int pivot = a[lo];
         int i = lo + 1, j;
         for (j = lo + 1; j < hi; j++) {
             if (a[j] < pivot) {
-                swap(a, i, j);
+                swap(a, j, i);
                 i++;
             }
         }
-        swap(a, lo, i - 1);
+        swap(a, lo, i-1);
         return i;
-    }
-
-    /**
-     * Returns pivot element
-     * @param a input array
-     * @return
-     */
-    private static int findPivot(int[] a, int lo) {
-        return lo;
     }
 
     private static void swap(int[] a, int i, int j) {
         int save = a[i];
         a[i] = a[j];
         a[j] = save;
+    }
+
+    public static void main(String[] args) {
+        File f = new File("/home/artur/work/study/algo/data/QuickSort.txt");
+        int[] a = FileUtils.getIntArrayFromFile(f);
+        assert a.length != 0: "array is empty";
+        sort(a);
+        System.out.println("Input array length=" + a.length);
+        System.out.println("Comparisons: " + comparisons);
     }
 }
