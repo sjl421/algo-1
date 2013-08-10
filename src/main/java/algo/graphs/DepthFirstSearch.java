28b076f185f2abd6c49d53f19996a42c9e73efd4
Artur Khalikov <akhalikoff@gmail.com>
2013-08-10 17:32:27 +0400

    Update DepthFirstSearch.java

--- src/main/java/algo/graphs/DepthFirstSearch.java
+++ src/main/java/algo/graphs/DepthFirstSearch.java
@@ -7,24 +7,46 @@ import java.util.*;
  *
  * @author: Artur Khalikov
  */
-class DepthFirstSearch extends AbstractGraphSearch {
+class DepthFirstSearch {
 
-    private Deque<Vertex> stack;
+    private Graph graph;
+    private int start;
+    private boolean explored[];
+    private Map<Integer, Integer> edgeTo;
 
-    public DepthFirstSearch(Graph graph, Vertex start) {
-        super(graph);
-        stack = new LinkedList();
-        stack.add(start);
+    public DepthFirstSearch(Graph graph, int start) {
+        this.graph = graph;
+        this.start = start;
+        explored = new boolean[graph.size()];
+        edgeTo = new TreeMap();
+        search(start);
     }
 
-    public Vertex next() {
-        Vertex current = stack.pop();
-        for (Vertex v: graph.arcs(current)) {
-            if (!isExplored(v)) {
-                setExplored(v);
-                stack.push(v);
+    public Iterable<Integer> pathTo(int v) {
+        if (!graph.hasVertex(v)) return null;
+        if (!hasPathTo(v)) return null;
+        Deque<Integer> path = new LinkedList();
+        for (int x = v; x != start; x = edgeTo.get(x))
+            path.push(x);
+        path.push(start);
+        return path;
+    }
+
+    public boolean hasPathTo(int v) {
+        return explored[v-1];
+    }
+
+    /**
+     * DFS recoursive routine
+     * @param v Vertex
+     */
+    private void search(int v) {
+        explored[v-1] = true;
+        for (Integer w: graph.adj(v)) {
+            if (!explored[w-1]) {
+                edgeTo.put(w, v);
+                search(w);
             }
         }
-        return current;
     }
 }
