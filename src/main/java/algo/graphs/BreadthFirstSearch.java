70f9b79928cfad8598a180ac34d3982504ab80b2
Artur Khalikov <akhalikoff@gmail.com>
2013-08-10 17:10:14 +0400

    Update BreadthFirstSearch.java

--- src/main/java/algo/graphs/BreadthFirstSearch.java
+++ src/main/java/algo/graphs/BreadthFirstSearch.java
@@ -1,36 +1,51 @@
 package algo.graphs;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Breath-First Iterator
  *
  * @author Artur Khalikov
  */
-class BreadthFirstSearch extends AbstractGraphSearch {
+class BreadthFirstSearch {
 
-    private Queue<Vertex> queue;
+    private Graph graph;
+    private boolean explored[];
+    private Map<Integer, Integer> edgeTo;
+    private int start;
 
-    public BreadthFirstSearch(Graph graph, Vertex start) {
-        super(graph);
-        queue = new LinkedList();
-        search(start);
+    public BreadthFirstSearch(Graph graph, int start) {
+        this.graph = graph;
+        this.start = start;
+        explored = new boolean[graph.size()];
+        edgeTo = new TreeMap();
+        search(graph, start);
     }
 
-    public Iterable<Vertex> getResult() {
-        return queue;
+    public Iterable<Integer> pathTo(int v) {
+        if (!graph.hasVertex(v)) return null;
+        if (!hasPathTo(v)) return null;
+        Deque<Integer> path = new LinkedList();
+        for (int x = v; x != start; x = edgeTo.get(x))
+            path.push(x);
+        path.push(start);
+        return path;
     }
 
-    private void search(Vertex start) {
-        setExplored(start);
+    public boolean hasPathTo(int v) {
+        return explored[v-1];
+    }
+
+    private void search(Graph graph, int start) {
+        Queue<Integer> queue = new LinkedList();
+        explored[start-1] = true;
         queue.add(start);
         while (!queue.isEmpty()) {
-            Vertex v = queue.remove();
-            for (Vertex w: graph.arcs(v)) {
-                if (!isExplored(w)) {
-                    setExplored(w);
+            Integer v = queue.remove();
+            for (Integer w: graph.adj(v)) {
+                if (!explored[w-1]) {
+                    explored[w-1] = true;
+                    edgeTo.put(w, v);
                     queue.add(w);
                 }
             }
