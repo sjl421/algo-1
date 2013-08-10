7144fd5a04bff5e678d93ef1e83812bc4b2faa86
Artur Khalikov <akhalikoff@gmail.com>
2013-08-10 17:12:33 +0400

    Update BreadthFirstSearchTest.java

--- src/test/java/algo/graphs/BreadthFirstSearchTest.java
+++ src/test/java/algo/graphs/BreadthFirstSearchTest.java
@@ -1,7 +1,9 @@
 package algo.graphs;
 
-import org.junit.Before;
 import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
 
 /**
  * Test {@link BreadthFirstSearch}
@@ -10,35 +12,53 @@ import org.junit.Test;
  */
 public class BreadthFirstSearchTest {
 
-    private DiGraph dg1;
+    @Test
+    public void test1() throws Exception {
+        System.out.println("Test graph 1");
+        DiGraph g = new DiGraph(4);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(2, 4);
+        g.addEdge(3, 1);
+        g.addEdge(3, 4);
+        
+        System.out.println("Test starting from 1");
+        BreadthFirstSearch bfs = new BreadthFirstSearch(g, 1);
+        Iterable<Integer> path0 = bfs.pathTo(6);
+        assertNull(path0);
 
-    @Before
-    public void setUp() throws Exception {
-        dg1 = new DiGraph(4);
-        dg1.addEdge(1, 2);
-        dg1.addEdge(2, 3);
-        dg1.addEdge(2, 4);
-        dg1.addEdge(3, 1);
-        dg1.addEdge(3, 4);
+        Iterable<Integer> path4 = bfs.pathTo(4);
+        assertNotNull(path4);
+        print(path4);
+
+        Iterable<Integer> path3 = bfs.pathTo(3);
+        assertNotNull(path3);
+        print(path3);
+
+        Iterable<Integer> path2 = bfs.pathTo(2);
+        assertNotNull(path2);
+        print(path2);
     }
 
     @Test
-    public void testNext() throws Exception {
-        System.out.println("Test starting from 1");
-        print(dg1.breathFirstSearch(new Vertex(1)));
+    public void test2() throws Exception {
+        System.out.println("Test graph 2");
+        DiGraph g = new DiGraph(6);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 6);
+        g.addEdge(3, 4);
+        g.addEdge(3, 5);
 
-        System.out.println("Test starting from 2");
-        print(dg1.breathFirstSearch(new Vertex(2)));
-
-        System.out.println("Test starting from 3");
-        print(dg1.breathFirstSearch(new Vertex(3)));
-
-        System.out.println("Test starting from 4");
-        print(dg1.breathFirstSearch(new Vertex(4)));
+        BreadthFirstSearch bfs = new BreadthFirstSearch(g, 1);
+        print(bfs.pathTo(5));
+        print(bfs.pathTo(4));
+        print(bfs.pathTo(6));
     }
 
-    private void print(Iterable<Vertex> vertices) {
-        for (Vertex v: vertices)
-            System.out.println(v);
+    private void print(Iterable<Integer> vertices) {
+        for (Integer v: vertices)
+            System.out.print(v + " ");
+        System.out.println();
     }
 }
