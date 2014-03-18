0ad79b662b5f55917b8341f36c451c6bffb2c4f6
Artur Khalikov <akhalikoff@gmail.com>
2014-03-18 12:02:41 +0000

    Update Fibo.java

--- src/main/java/algo/Fibo.java
+++ src/main/java/algo/Fibo.java
@@ -1,5 +1,19 @@
+// TODO: test
+
 public class Fibo {
+    
+    private BigDecimal prev1 = BigDecomal.ZERO; 
+    private BigDecimal prev2 = BigDecimal.ONE;
+    
     public static void main(String[] args) {
         
+    }
+    
+    
+    public BigDecimal next() {
+        BigDecimal val = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = val;
+        return val;
     }
 }
