2cd89545ec1f9736f8dc2d3ae1ddef134be6bcec
Artur Khalikov <akhalikoff@gmail.com>
2014-12-26 23:57:04 +0200

    project refactoring

---
+++ src/test/java/stanford/part1/sort/QuickSort3Test.java
@@ -0,0 +1,85 @@
+package stanford.part1.sort;
+
+import org.junit.Test;
+import util.ArrayUtils;
+import util.InputFile;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static stanford.part1.sort.QuickSort3.findPivot;
+
+/**
+ * Test for {@link QuickSort3}
+ * @author: Artur Khalikov
+ */
+public class QuickSort3Test {
+
+    @Test
+    public void testSortOneElement() throws Exception {
+        int[] actual = {1};
+        int[] expected = {1};
+        QuickSort3.sort(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSort() throws Exception {
+        int[] actual = {5, 4, 1, 8, 7, 2, 6, 3};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8};
+        QuickSort3.sort(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSort2() throws Exception {
+        int n = 10000 + 1;
+        int[] expected = new int[n];
+        ArrayUtils.fillFrom(expected, 0);
+
+        int[] actual = Arrays.copyOf(expected, expected.length);
+        ArrayUtils.shuffle(actual);
+        QuickSort3.sort(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindPivot() throws Exception {
+        int[] a = {4, 5, 6, 7};
+        int expected = 1;
+        int actual = findPivot(a, 0, a.length-1);
+        assertEquals(expected, actual);
+        assertEquals(5, a[actual]);
+
+        a = new int[] {5, 7, 10, 2, 3};
+        expected = 0;
+        actual = findPivot(a, 0, a.length-1);
+        assertEquals(expected, actual);
+        assertEquals(5, a[actual]);
+    }
+
+    @Test
+    public void testComparisons1() throws Exception {
+        int[] a = {3, 9, 8, 4, 6, 10, 2, 5, 7, 1};
+        int expected = 21;
+        QuickSort3.sort(a);
+        assertEquals(expected, QuickSort3.comparisons);
+    }
+
+    @Test
+    public void testComparisons2() throws Exception {
+        int[] a = new InputFile("/home/artur/work/study/algo/data/100.txt").getIntegerArray();
+        int expected = 518;
+        QuickSort3.sort(a);
+        assertEquals(expected, QuickSort3.comparisons);
+    }
+
+    @Test
+    public void testComparisons3() throws Exception {
+        int[] a = new InputFile("/home/artur/work/study/algo/data/1000.txt").getIntegerArray();
+        int expected = 8921;
+        QuickSort3.sort(a);
+        assertEquals(expected, QuickSort3.comparisons);
+    }
+}
---
+++ src/test/java/stanford/part1/sort/QuickSortTest.java
@@ -0,0 +1,69 @@
+package stanford.part1.sort;
+
+import org.junit.Test;
+import util.ArrayUtils;
+import util.InputFile;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Test for {@link stanford.part1.sort.QuickSort}
+ * @author: Artur Khalikov
+ */
+public class QuickSortTest {
+
+    @Test
+    public void testSortOneElement() throws Exception {
+        int[] actual = {1};
+        int[] expected = {1};
+        QuickSort.sort(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSort() throws Exception {
+        int[] actual = {5, 4, 1, 8, 7, 2, 6, 3};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8};
+        QuickSort.sort(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSort2() throws Exception {
+        int n = 10000 + 1;
+        int[] expected = new int[n];
+        ArrayUtils.fillFrom(expected, 0);
+
+        int[] actual = Arrays.copyOf(expected, expected.length);
+        ArrayUtils.shuffle(actual);
+        QuickSort.sort(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testComparisons1() throws Exception {
+        int[] a = {3, 9, 8, 4, 6, 10, 2, 5, 7, 1};
+        int expected = 25;
+        QuickSort.sort(a);
+        assertEquals(expected, QuickSort.comparisons);
+    }
+
+    @Test
+    public void testComparisons2() throws Exception {
+        int[] a = new InputFile("/home/artur/work/study/algo/data/100.txt").getIntegerArray();
+        int expected = 615;
+        QuickSort.sort(a);
+        assertEquals(expected, QuickSort.comparisons);
+    }
+
+    @Test
+    public void testComparisons3() throws Exception {
+        int[] a = new InputFile("/home/artur/work/study/algo/data/1000.txt").getIntegerArray();
+        int expected = 10297;
+        QuickSort.sort(a);
+        assertEquals(expected, QuickSort.comparisons);
+    }
+}
