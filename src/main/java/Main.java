16eeeffd02e73168d007d6f2c203f3b538b06b94
artur <akhalikoff@gmail.com>
2015-02-11 22:43:25 +0300

    pom updated

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by artur on 09.02.15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //ArrayList<String> list = Lists.newArrayList("1", "2", "3", "4", "5");
+        //list.remove(3);
+    }
+}
