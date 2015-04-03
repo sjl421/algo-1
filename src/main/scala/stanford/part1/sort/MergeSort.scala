d38b9beb26105778c66a2e30fe7808dc6c396d93
artur <akhalikoff@gmail.com>
2015-04-03 16:56:54 +0300

    merge sort @ scala

---
+++ src/main/scala/stanford/part1/sort/MergeSort.scala
@@ -0,0 +1,11 @@
+package stanford.part1.sort
+
+object MergeSort {
+	def sort(a: Array[Int]): Array[Int] {
+		sort(a, 0, a.length);
+	}
+
+	def sort(a: Array[Int], from: Int, to: Int) {
+		if (to < from)
+	}					
+}
