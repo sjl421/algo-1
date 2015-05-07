29f5d5986e2a12929d12087b63fa955553186925
Artur Khalikov <akhalikoff@gmail.com>
2015-05-07 19:50:33 +0300

    codeeval: two easy challenges

--- src/main/java/codeeval/moderate/RemoveCharacters.java
+++ src/main/java/codeeval/moderate/RemoveCharacters.java
@@ -1,10 +1,33 @@
-package codeeval.easy;
+package codeeval.moderate;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Write a program which removes specific characters from a string.
  */
 public class RemoveCharacters {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+       	BufferedReader reader = new BufferedReader(new FileReader(new File(args[0])));
+		String line;
+       	while ((line = reader.readLine()) != null) {
+       		String[] tokens = line.split(",");
+       		Set scrub = new HashSet();
+       		
+       		for (char c: tokens[1].trim().toCharArray())
+       			scrub.add(c);
 
+       		StringBuilder sb = new StringBuilder();
+       		for (char c: tokens[0].trim().toCharArray()) {
+       			if (!scrub.contains(c)) {
+       				sb.append(c);
+       			}
+       		}
+       		System.out.println(sb.toString());
+       	}
+       	reader.close();
     }
 }
--- src/main/java/codeeval/moderate/TrailingString.java
+++ src/main/java/codeeval/moderate/TrailingString.java
@@ -1,11 +1,57 @@
 package codeeval.moderate;
 
+import java.io.*;
+
 /**
  * There are two strings: A and B.
  * Print 1 if string B occurs at the end of string A. Otherwise, print 0.
  */
 public class TrailingString {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+    	BufferedReader reader = new BufferedReader(new FileReader(new File(args[0])));
+    	String line;
+    	while ((line = reader.readLine()) != null) {
+    		String[] sentences = line.split(",");
+    		System.out.println(isTrailingString(sentences[0], sentences[1]) ? 1: 0);
+    	}
+    	reader.close();
     }
-}
+
+	/**
+	 * O(n), where n = length(str)
+	 * @param subject
+	 * @param str
+	 * @return
+	 */
+    static boolean isTrailingString(String subject, String str) {
+    	if (str.length() > subject.length())
+    		return false;
+		int j = subject.length()-1;
+    	for (int i = str.length()-1; i >= 0; i--) {
+    		if (str.charAt(i) != subject.charAt(j--))
+    			return false;
+    	}
+    	return true;
+    }
+
+	/**
+	 * This solution does not work on some tests, why ?
+	 * @param subject
+	 * @param str
+	 * @return
+	 */
+	static boolean isTrailingString2(String subject, String str) {
+		if (str.length() > subject.length())
+			return false;
+		String[] strTokens = str.split(" ");
+		String[] subjectTokens = subject.split(" ");
+		if (strTokens.length > subjectTokens.length)
+			return false;
+		int j = subjectTokens.length-1;
+		for (int i = strTokens.length-1; i >= 0; i--) {
+			if (strTokens[i].hashCode() != subjectTokens[j--].hashCode())
+				return false;
+		}
+		return true;
+	}
+ }
