0d0af6a45383afa7592c510ade8df7e263a0cdd1
Artur Khalikov <akhalikoff@gmail.com>
2015-05-16 02:01:53 +0300

    algo: binary calculator

--- src/main/java/algo/recursion/BinaryCalculator.java
+++ src/main/java/algo/recursion/BinaryCalculator.java
@@ -8,19 +8,18 @@ class BinaryCalculator {
      * O(log n) - the time and space requirements are estimated
      * by the number of recursive calls.
      */
-    public static String getBinary(int decimalNum) {
+    public static String getBinary(long decimalNum) {
         if (decimalNum < 0)
             throw new IllegalArgumentException("Expected only positive numbers");
 
         return calculateRecursively(decimalNum);
     }
 
-    private static String calculateRecursively(int decimalNum) {
+    private static String calculateRecursively(long num) {
         // Simplest case
-        if (decimalNum <= 1)
-            return Integer.toString(decimalNum);
+        if (num <= 1)
+            return Long.toString(num);
         else
-            return calculateRecursively(decimalNum / 2)
-                    + Integer.toString(decimalNum % 2);
+            return calculateRecursively(num / 2) + Long.toString(num % 2);
     }
 }
