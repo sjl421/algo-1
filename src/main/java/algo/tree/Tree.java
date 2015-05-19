3bda989abc03144ca896d323e43234a439a3d131
Arthur Khalikov <akhalikoff@gmail.com>
2015-05-19 22:07:59 +0300

    Update Tree.java

--- src/main/java/algo/tree/Tree.java
+++ src/main/java/algo/tree/Tree.java
@@ -17,7 +17,7 @@ public class Tree<K> {
     }
 
     public void insert(K key) {
-
+        recursuveInsert(root, null, key);
     }
 
     private Entry<K> recursiveSearch(Entry<K> node, K key) {
@@ -32,6 +32,22 @@ public class Tree<K> {
             return (cmp < 0)
                     ? recursiveSearch(node.left, key)
                     : recursiveSearch(node.right, key);
+    }
+    
+    private void recursuveInsert(Entry<K> node, Entry<K> parent, K key) {
+        if (node == null) {
+            Entry<K> newEntry = new Entry();
+            newEntry.key = key;
+            newEntry.left = newEntry.right = null;
+            newEntry.parent = parent;
+        } else {
+            Comparable k = (Comparable) key;
+            int cmp = k.compareTo(node.key);
+            if (cmp < 0)
+                recursuveInsert(node.left, );
+            else
+                recursuveInsert();
+        }
     }
 
     static final class Entry<K> {
