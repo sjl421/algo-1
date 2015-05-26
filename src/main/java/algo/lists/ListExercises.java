89745da23d74fce785ac9f11ed7cdad0cd26270b
Artur Khalikov <akhalikoff@gmail.com>
2015-05-27 02:00:43 +0300

    algo: merging sorted linked lists

--- src/main/java/algo/lists/ListExercises.java
+++ src/main/java/algo/lists/ListExercises.java
@@ -52,4 +52,42 @@ public class ListExercises {
         p.next.next = p;
         p.next = null;
     }
+
+    /**
+     * Merging of sorted linked lists
+     */
+    static Node mergeLists(Node list1, Node list2) {
+        if (list1 == null && list2 == null)
+            return null;
+        if (list1 == null && list2 != null)
+            return list2;
+        if (list1 != null && list2 == null)
+            return list1;
+
+        Node temp = null, head2 = null, p1 = list1, p2 = list2;
+        while (p1 != null || p2 != null) {
+            if (p1 != null && p2 == null) {
+                if (temp != null)
+                    temp.next = p1;
+                else
+                    temp = p1;
+            } else if (p1 == null && p2 != null) {
+                if (temp != null)
+                    temp.next = p2;
+                else
+                    temp = p2;
+            } else if (p1.item < p2.item) {
+                temp = p1;
+                temp.next = p2;
+            } else {
+                temp = p2;
+                temp.next = p1;
+            }
+            if (p1 != null) p1 = p1.next;
+            if (p2 != null) p2 = p2.next;
+            if (head2 == null) head2 = temp;
+            if (temp.next != null) temp = temp.next.next;
+        }
+        return head2;
+    }
 }
