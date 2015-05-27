42682e9fd41fe2d67d3ea339648e09fcb041c46d
Arthur Khalikov <akhalikoff@gmail.com>
2015-05-27 12:34:19 +0300

    Update ListExercises.java

--- src/main/java/algo/lists/ListExercises.java
+++ src/main/java/algo/lists/ListExercises.java
@@ -57,37 +57,14 @@ public class ListExercises {
      * Merging of sorted linked lists
      */
     static Node mergeLists(Node list1, Node list2) {
-        if (list1 == null && list2 == null)
-            return null;
-        if (list1 == null && list2 != null)
-            return list2;
-        if (list1 != null && list2 == null)
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1.item < list2.item) {
+            list1.next = mergeLists(list1.next, list2); 
             return list1;
-
-        Node temp = null, head2 = null, p1 = list1, p2 = list2;
-        while (p1 != null || p2 != null) {
-            if (p1 != null && p2 == null) {
-                if (temp != null)
-                    temp.next = p1;
-                else
-                    temp = p1;
-            } else if (p1 == null && p2 != null) {
-                if (temp != null)
-                    temp.next = p2;
-                else
-                    temp = p2;
-            } else if (p1.item < p2.item) {
-                temp = p1;
-                temp.next = p2;
-            } else {
-                temp = p2;
-                temp.next = p1;
-            }
-            if (p1 != null) p1 = p1.next;
-            if (p2 != null) p2 = p2.next;
-            if (head2 == null) head2 = temp;
-            if (temp.next != null) temp = temp.next.next;
+        } else {
+            list2.next = mergeLists(list1, list2.next); 
+            return list2;
         }
-        return head2;
     }
 }
