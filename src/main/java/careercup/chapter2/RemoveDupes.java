76f363c75c62d0e0cbd9fea7e172a42979021521
Artur Khalikov <akhalikoff@gmail.com>
2015-06-08 23:33:12 +0300

    career cup: challenge 2.1

--- src/main/java/careercup/chapter2/RemoveDupes.java
+++ src/main/java/careercup/chapter2/RemoveDupes.java
@@ -1,5 +1,7 @@
 package careercup.chapter2;
 
+import java.util.HashSet;
+
 /**
  * Write code to remove duplicates from an unsorted linked list (single-linked list).
  * FOLLOW UP
@@ -7,8 +9,41 @@ package careercup.chapter2;
  *
  * @author ahalikov
  */
-public class RemoveDupes {
+class RemoveDupes {
+    public static class Node {
+        int data;
+        Node next;
 
+        public Node() {
+        }
 
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
 
+    /**
+     * Solution using hash-table to track duplicate elements
+     */
+    static Node removeDupes(Node head) {
+        if (head == null || head.next == null)
+            return head;
+
+        Node curr = head.next,
+             prev = head;
+        HashSet<Integer> seen = new HashSet<>();
+        while (curr != null) {
+            if (seen.contains(curr.data)) {
+                // removing duplicate element
+                prev.next = curr.next;
+                curr.next = null;
+            } else {
+                seen.add(curr.data);
+            }
+            prev = curr;
+            curr = curr.next;
+        }
+        return head;
+    }
 }
