c15bfb2a361f35850e54a5228d9637f199016bd0
Artur Khalikov <akhalikoff@gmail.com>
2015-08-24 23:14:25 +0300

    matrix column sort problem added

---
+++ src/main/java/algo/MatrixColumnSorter.java
@@ -0,0 +1,19 @@
+package algo;
+
+/**
+ * You have a numeric (m, n) matrix.
+ * We will call sum of modules of negative elements in a column as column characteristic.
+ *
+ * Input: two integer numbers m, n
+ * Randomly generated numeric matrix of size (m, n).
+ *
+ * Output: matrix sorted in ascending order of column characteristic.
+ *
+ * @author ahalikov
+ */
+public class MatrixColumnSorter {
+
+    static int[][] sort(int[][] matrix) {
+        return matrix;
+    }
+}
