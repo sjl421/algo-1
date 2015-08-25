4f852e38e302a483819ea63d906e14e1f17c533e
Arthur Khalikov <akhalikoff@gmail.com>
2015-08-25 23:57:12 +0300

    Update MatrixColumnSorter.java

--- src/main/java/algo/MatrixColumnSorter.java
+++ src/main/java/algo/MatrixColumnSorter.java
@@ -2,12 +2,12 @@ package algo;
 
 /**
  * You have a numeric (m, n) matrix.
- * We will call sum of modules of negative elements in a column as column characteristic.
+ * We will call sum of modules (abs) of negative elements in a column as column characteristic.
  *
  * Input: two integer numbers m, n
  * Randomly generated numeric matrix of size (m, n).
  *
- * Output: matrix sorted in ascending order of column characteristic.
+ * Output: matrix, which columns sorted in ascending order of column characteristic.
  *
  * @author ahalikov
  */
