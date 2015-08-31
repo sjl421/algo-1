d5fd0932bc9bddc7cc4522c2ea4e4fe8a9269a7b
Artur Khalikov <akhalikoff@gmail.com>
2015-08-31 15:04:16 +0300

    sql review

---
+++ src/main/sql/Population.sql
@@ -0,0 +1,52 @@
+/**
+
+  Average Population
+
+  Given a City table, whose fields are described as:
+
+  +-------------+----------+
+  | Field       | Type     |
+  +-------------+----------+
+  | ID          | int(11)  |
+  | Name        | char(35) |
+  | CountryCode | char(3)  |
+  | District    | char(20) |
+  | Population  | int(11)  |
+  +-------------+----------+
+
+  1. write a query that will fetch all columns for every row in the table.
+*/
+select ID, Name, CountryCode, District, Population from City;
+
+/*
+ * 2. print all the details of the city with ID is 1661.
+ */
+select * from City where ID = 1661;
+
+/*
+ * 3. You have to print all the details of all the cities of Japan.
+ * The CountryCode for Japan is "JPN".
+ */
+select * from City where CountryCode='JPN';
+
+/*
+ * 4. You have to print the average population of all cities,
+ * rounded down to the nearest integer.
+ */
+select round(avg(Population)) from City;
+
+/*
+ * 5. Japan Population
+ * you have to print the sum of population of all the cities of Japan.
+ * The CountryCode for Japan is "JPN".
+ */
+select sum(Population)
+from City
+where CountryCode = 'JPN';
+
+/*
+ * 6. Population Density Difference
+ * Print the difference between the maximum and minimum city populations.
+ */
+select max(Population) - min(Population)
+from City;
---
+++ src/main/sql/WeatherObservation.sql
@@ -0,0 +1,90 @@
+/**
+ Given a table STATION that holds data for five fields namely
+ ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
+ +-------------+------------+
+ | Field       |   Type     |
+ +-------------+------------+
+ | ID          | INTEGER    |
+ | CITY        | VARCHAR(21)|
+ | STATE       | VARCHAR(2) |
+ | LAT_N       | NUMERIC    |
+ | LONG_W      | NUMERIC    |
+ +-------------+------------+
+*/
+
+/* 1
+ * Write a query to print the list of CITY in lexicographical order
+ * for even ID only. Do not print duplicates.
+ */
+select distinct CITY from STATION
+where mod(ID, 2) = 0
+order by CITY;
+
+/* 2
+ * Let NUM be no. of cities and NUM_unique be no. of unique cities,
+ * then write a query to print the value of NUM - NUM_unique.
+ */
+select count(CITY) - count(distinct CITY)
+from STATION;
+
+/* 3
+ * Let |city| be the length of the city, write a query to print two lines:
+ * 1. First line is city1 and |city1| separated by space, where |city1| is the possible minimum value.
+ * 2. Second line is city2 and |city2| separated by space, where |city2|  is the possible maximum value.
+ * If there are more than one possible cities print the lexicographical smallest.
+ */
+select * from (
+    select CITY || ' ' || length(CITY)
+    from STATION
+    where length(CITY) = (select min(length(CITY)) from STATION)
+    order by CITY asc
+) where rownum <= 1
+union all
+select * from (
+    select CITY || ' ' || length(CITY)
+    from STATION
+    where length(CITY) = (select max(length(CITY)) from STATION)
+    order by CITY asc
+) where rownum <= 1;
+
+/* 4
+ * Write a query to print the list of CITY that start with vowels in lexicographical order.
+ * Do not print duplicates.
+ */
+select distinct CITY
+from STATION
+where regexp_like(CITY, '^[aeiou]', 'i')
+order by CITY asc;
+
+-- or
+
+select distinct CITY
+from STATION
+where substr(lower(CITY), 0, 1) in ('a', 'e', 'i', 'o', 'u')
+order by CITY asc;
+
+/* 5
+ * Write a query to print the list of CITY that ends at vowels in lexicographical order.
+ * Do not print duplicates.
+ */
+select distinct CITY
+from STATION
+where regexp_like(CITY, '[aeiou]$', 'i')
+order by CITY asc;
+
+-- or
+
+select distinct CITY
+from STATION
+where substr(lower(CITY), -1, 1) in ('a', 'e', 'i', 'o', 'u')
+order by CITY asc;
+
+/* 6
+ * Write a query to print the list of CITY that starts with vowels and ends at vowels
+ * in lexicographical order. Do not print duplicates.
+ */
+select distinct CITY
+from STATION
+where substr(lower(CITY), 0, 1) in ('a', 'e', 'i', 'o', 'u')
+  and substr(lower(CITY), -1, 1) in ('a', 'e', 'i', 'o', 'u')
+order by CITY asc;
