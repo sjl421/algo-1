733d5bdda81406dff71cbf3035427dc4e4897e99
Arthur Khalikov <akhalikoff@gmail.com>
2015-09-21 16:12:31 +0300

    Update StackImplementation.java

--- src/main/java/codeeval/moderate/stack/StackImplementation.java
+++ src/main/java/codeeval/moderate/stack/StackImplementation.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
 /**
  * Stack Implementation
  * Write a program which implements a stack interface for integers.
- * The interface should have ‘push’ and ‘pop’ functions.
- * Your task is to ‘push’ a series of integers and then ‘pop’ and print every alternate integer.
+ * The interface should have 'push' and 'pop' functions.
+ * Your task is to 'push' a series of integers and then 'pop' and print every alternate integer.
  */
 public class StackImplementation {
     public static void main(String[] args) throws Exception {
