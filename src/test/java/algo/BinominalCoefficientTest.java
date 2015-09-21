b9e2511968c4aead0d2ad565775e7e7ed00418e7
Arthur Khalikov <akhalikoff@gmail.com>
2015-09-21 17:33:29 +0300

    Create BinominalCoefficientTest.java

---
+++ src/test/java/algo/BinominalCoefficientTest.java
@@ -0,0 +1,16 @@
+package algo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by akhalikov on 21.09.2015.
+ */
+public class BinominalCoefficientTest {
+
+    @Test
+    public void testBinom() throws Exception {
+        BinominalCoefficient.binom(4, 4);
+    }
+}
