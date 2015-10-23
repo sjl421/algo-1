424582952821671a2a05aaee55ff33b806fbb2c7
Artur Khalikov <akhalikoff@gmail.com>
2015-10-23 14:56:28 +0300

    selection sort

---
+++ src/test/java/algo/sorting/SelectionSortTest.java
@@ -0,0 +1,32 @@
+package algo.sorting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test for {@link SelectionSort}
+ *
+ * @author ahalikov
+ */
+public class SelectionSortTest {
+
+    @Test
+    public void testSort() throws Exception {
+        int[] actual = {12, 11, 13, 5, 6};
+        int[] expected = {5, 6, 11, 12, 13};
+
+        SelectionSort.sort(actual);
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSort1() throws Exception {
+        int[] actual = {0, 0, 0};
+        int[] expected = {0, 0, 0};
+
+        SelectionSort.sort(actual);
+        Assert.assertArrayEquals(expected, actual);
+    }
+}
