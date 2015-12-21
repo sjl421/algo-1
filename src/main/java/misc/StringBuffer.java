f027f51b75951c1c4ec32258b208dbc2ad02b33a
Artur Khalikov <akhalikoff@gmail.com>
2015-12-21 23:59:48 +0300

    add string buffer impl

---
+++ src/main/java/misc/StringBuffer.java
@@ -0,0 +1,41 @@
+package misc;
+
+/**
+ * Implementation of string buffer
+ *
+ * @author ahalikov
+ */
+public class StringBuffer {
+  private char[] buffer;
+
+  public StringBuffer() {
+  }
+
+  public StringBuffer append(char c) {
+    return null;
+  }
+
+  public StringBuffer append(char[] str) {
+    return null;
+  }
+
+  public StringBuffer append(String str) {
+    return null;
+  }
+
+  public StringBuffer insert(int offset, char c) {
+    return null;
+  }
+
+  public StringBuffer insert(int offset, char[] str) { return null; }
+  public StringBuffer insert(int offset, String str) { return null; }
+  public StringBuffer replace(int start, int end, String str) { return null; }
+
+  public int length() {
+    return 0;
+  }
+
+  public String toString() {
+    return "";
+  }
+}
