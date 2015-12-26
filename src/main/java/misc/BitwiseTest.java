297321d0974a93e1187fd91c74a74ab93320b4e7
Artur Khalikov <akhalikoff@gmail.com>
2015-12-26 23:14:28 +0300

    add dynamic array demo

---
+++ src/main/java/misc/BitwiseTest.java
@@ -0,0 +1,14 @@
+package misc;
+
+/**
+ * @author akhalikov
+ */
+public class BitwiseTest {
+  public static void main(String[] args) {
+    System.out.println(4 >> 1);
+    System.out.println(8 >> 1);
+    System.out.println(31 >> 1);
+    System.out.println("---------");
+    System.out.println(1 << 1);
+  }
+}
