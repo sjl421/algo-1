c258ea86b4118596cb9624989a2640066bf9884b
Artur Khalikov <akhalikoff@gmail.com>
2016-01-02 22:58:54 +0300

    review linked list problems from CareerCup

--- src/main/java/careercup/chapter2/NthToTheLast.java
+++ src/main/java/careercup/chapter2/NthToTheLast.java
@@ -5,31 +5,34 @@ package careercup.chapter2;
  */
 public class NthToTheLast {
 
-    static class Node {
-        int data;
-        Node next;
+  static class Node {
+    int data;
+    Node next;
 
-        public Node() {
-        }
-
-        public Node(int data, Node next) {
-            this.data = data;
-            this.next = next;
-        }
+    public Node(int data, Node next) {
+      this.data = data;
+      this.next = next;
     }
+  }
 
-    static Node find(Node head, int n) {
-        if (head == null || n < 1) return null;
-        Node p1 = head, p2 = head;
-        for (int i = 0; i < n - 1; i++) {
-            if (p2 == null)
-                 return null;
-            p2 = p2.next;
-        }
-        while (p2.next != null) {
-            p1 = p1.next;
-            p2 = p2.next;
-        }
-        return p1;
+  /**
+   * Time complexity: O(n)
+   * Space complexity: O(1)
+   */
+  static Node find(Node head, int n) {
+    if (head == null)
+      return null;
+
+    Node p1 = head, p2 = head;
+    for (int i = 0; i < n - 1; i++) {
+      if (p2 == null)
+        return null;
+      p2 = p2.next;
     }
+    while (p2.next != null) {
+      p1 = p1.next;
+      p2 = p2.next;
+    }
+    return p1;
+  }
 }
--- src/test/java/careercup/chapter2/RemoveDupesTest.java
+++ src/test/java/careercup/chapter2/RemoveDupesTest.java
@@ -1,42 +1,73 @@
 package careercup.chapter2;
 
 import careercup.chapter2.RemoveDupes.Node;
+
 import static junit.framework.Assert.*;
+
 import org.junit.Test;
 
+/**
+ * Test for {@link RemoveDupes}
+ */
 public class RemoveDupesTest {
 
-    @Test
-    public void testRemoveDupesEmptyList() throws Exception {
-        assertNull(RemoveDupes.removeDupes(null));
+  @Test
+  public void testRemoveDupesEmptyList() throws Exception {
+    Node list = new Node(1, null);
+    RemoveDupes.removeDupes(list);
+    assertEquals(1, list.data);
+    assertEquals(null, list.next);
+  }
 
-        Node list = new Node(1, null);
-        assertEquals(list, RemoveDupes.removeDupes(list));
-    }
+  @Test
+  public void testRemoveDupes() throws Exception {
+    Node list = new Node(6, new Node(5, new Node(3,
+      new Node(2, new Node(4, new Node(1, new Node(2, null)))))));
 
-    @Test
-    public void testRemoveDupes() throws Exception {
-        Node list = new Node(6, new Node(5, new Node(3,
-                new Node(2, new Node(4, new Node(1, new Node(2, null)))))));
+    RemoveDupes.removeDupes(list);
+    assertEquals(6, list.data);
+    Node p = list.next;
+    assertEquals(5, p.data);
 
-        Node result = RemoveDupes.removeDupes(list);
-        assertEquals(6, result.data);
-        Node p = result.next;
-        assertEquals(5, p.data);
+    p = p.next;
+    assertEquals(3, p.data);
 
-        p = p.next;
-        assertEquals(3, p.data);
+    p = p.next;
+    assertEquals(2, p.data);
 
-        p = p.next;
-        assertEquals(2, p.data);
+    p = p.next;
+    assertEquals(4, p.data);
 
-        p = p.next;
-        assertEquals(4, p.data);
+    p = p.next;
+    assertEquals(1, p.data);
 
-        p = p.next;
-        assertEquals(1, p.data);
+    p = p.next;
+    assertNull(p);
+  }
 
-        p = p .next;
-        assertNull(p);
-    }
+  @Test
+  public void testRemoveDupes2() throws Exception {
+    Node list = new Node(6, new Node(5, new Node(3,
+      new Node(2, new Node(4, new Node(1, new Node(2, null)))))));
+
+    RemoveDupes.removeDupes2(list);
+    assertEquals(6, list.data);
+    Node p = list.next;
+    assertEquals(5, p.data);
+
+    p = p.next;
+    assertEquals(3, p.data);
+
+    p = p.next;
+    assertEquals(2, p.data);
+
+    p = p.next;
+    assertEquals(4, p.data);
+
+    p = p.next;
+    assertEquals(1, p.data);
+
+    p = p.next;
+    assertNull(p);
+  }
 }
