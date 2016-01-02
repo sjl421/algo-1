7babd15a7f82c01b0d33fd25daf9d2943ac28e61
Artur Khalikov <akhalikoff@gmail.com>
2016-01-02 15:58:53 +0300

    review 'zero matrix' problem from CareerCup

--- src/test/java/careercup/chapter1/ZeroMatrixTest.java
+++ src/test/java/careercup/chapter1/ZeroMatrixTest.java
@@ -4,16 +4,16 @@ import org.junit.Test;
 import com.ahalikov.toolkit.utils.ArrayUtils;
 
 /**
- * Test for {@link MakeMyMatrixTotallyZero}
+ * Test for {@link ZeroMatrix}
  *
  * @author ahalikov
  */
-public class MakeMyMatrixTotallyZeroTest {
+public class ZeroMatrixTest {
 
     @Test
     public void testProcess1() throws Exception {
         int[][] m = {{1,2,3}, {5,0,4}, {1,2,3}};
-        int[][] result = MakeMyMatrixTotallyZero.process(m);
+        int[][] result = ZeroMatrix.makeZeros(m);
         int[][] expect = {{1,0,3}, {0,0,0}, {1,0,3}};
         ArrayUtils.assertMatrixEquals(expect, result);
     }
@@ -21,7 +21,7 @@ public class MakeMyMatrixTotallyZeroTest {
     @Test
     public void testProcess2() throws Exception {
         int[][] m = {{1,2,3}, {5,0,4}, {1,2,0}};
-        int[][] result = MakeMyMatrixTotallyZero.process(m);
+        int[][] result = ZeroMatrix.makeZeros(m);
         int[][] expect = {{1,0,0}, {0,0,0}, {0,0,0}};
         ArrayUtils.assertMatrixEquals(expect, result);
     }
@@ -29,7 +29,7 @@ public class MakeMyMatrixTotallyZeroTest {
     @Test
     public void testProcess3() throws Exception {
         int[][] m = {{0,2}, {4,0}};
-        int[][] result = MakeMyMatrixTotallyZero.process(m);
+        int[][] result = ZeroMatrix.makeZeros(m);
         int[][] expect = {{0,0}, {0,0}};
         ArrayUtils.assertMatrixEquals(expect, result);
     }
