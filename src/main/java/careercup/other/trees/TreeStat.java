915f6bff44e80fa4f39183d24a17f4696b215321
Artur Khalikov <akhalikoff@gmail.com>
2016-01-09 21:57:09 +0300

    Add some new questions

---
+++ src/main/java/careercup/other/trees/TreeStat.java
@@ -0,0 +1,13 @@
+package careercup.other.trees;
+
+/**
+ * 1. For binary tree with integer data, count the number
+ *    of nodes whose value is lower that the value of its upper nodes.
+ *
+ * 2. Given a binary tree, write a function calculates
+ *    sum of values at odd height minus sum of values at even height.
+ *
+ * @author akhalikov
+ */
+public class TreeStat {
+}
