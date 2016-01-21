6b1d4bc2fa35ae55d91f51abd1846625abd978b7
Artur Khalikov <akhalikoff@gmail.com>
2016-01-22 02:04:17 +0300

    Fix Sorted merge

--- src/main/java/careercup/chapter10/SortedMerge.java
+++ src/main/java/careercup/chapter10/SortedMerge.java
@@ -7,15 +7,20 @@ package careercup.chapter10;
  * @author ahalikov
  */
 class SortedMerge {
-  static int[] merge(int[] a, int[] b, int n, int m) {
+  static int[] merge(int[] a, int[] b) {
     if (a == null && b == null)
       return null;
-    int i = n - 1, // index of the last item in a
-      j = m - 1; // index of the last item in b
+
+    int m = b.length,
+        n = a.length - m,
+        i = n - 1, // index of the last item in a
+        j = m - 1; // index of the last item in b
 
     for (int k = m + n - 1; k >= 0; k--)
       if (i < 0)
         a[k] = b[j--];
+      else if (j < 0)
+        a[k] = a[i--];
       else
         a[k] = (a[i] > b[j]) ? a[i--] : b[j--];
     return a;
--- src/test/java/careercup/chapter10/SortedMergeTest.java
+++ src/test/java/careercup/chapter10/SortedMergeTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertNull;
 public class SortedMergeTest {
   @Test
   public void testMerge0() throws Exception {
-    assertNull(SortedMerge.merge(null, null, 5, 4));
+    assertNull(SortedMerge.merge(null, null));
   }
 
   @Test
@@ -22,7 +22,17 @@ public class SortedMergeTest {
     int[] b = {-1, 4, 8, 11};
     assertArrayEquals(
       new int[]{-1, 1, 2, 4, 5, 6, 7, 8, 11},
-      SortedMerge.merge(a, b, 5, 4)
+      SortedMerge.merge(a, b)
+    );
+  }
+
+  @Test
+  public void testMerge2() throws Exception {
+    int[] a = new int[]{1, 2, 2, 3, 5, 0, 0, 0};
+    int[] b = {10, 12, 13};
+    assertArrayEquals(
+      new int[]{1, 2, 2, 3, 5, 10, 12, 13},
+      SortedMerge.merge(a, b)
     );
   }
 }
