21aaed3b2b71520f8663a5f666e1d9540c51b392
Artur Khalikov <akhalikoff@gmail.com>
2016-01-31 23:53:32 +0300

    Matrix rotation

--- src/main/java/careercup/chapter01/MatrixRotation.java
+++ src/main/java/careercup/chapter01/MatrixRotation.java
@@ -7,8 +7,33 @@ package careercup.chapter01;
  * Can you do this in place?
  */
 class MatrixRotation {
+  /**
+   * Time complexity: O(n^2)
+   * Space complexity: O(n)
+   * @param matrix
+   * @param n
+   */
+  static void rotate(int[][] matrix, int n) {
+    for (int layer = 0; layer < n/2; ++layer) {
+      int first = layer;
+      int last = n - 1 - layer;
+      for (int i = first; i < last; ++i) {
+        int offset = i - first;
+        // save top
+        int top = matrix[first][i];
 
-    static int[][] rotate(int[][] m) {
-        return m;
+        // left to top
+        matrix[first][i] = matrix[last - offset][first];
+
+        // bottom to left
+        matrix[last - offset][first] = matrix[last][last - offset];
+
+        // right to bottom
+        matrix[last][last - offset] = matrix[i][last];
+
+        // top to right
+        matrix[i][last] = top;
+      }
     }
+  }
 }
