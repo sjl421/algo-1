8e805cf69b8909852ef4e8c629c32b572869887c
Artur Khalikov <akhalikoff@gmail.com>
2016-02-05 01:52:11 +0300

    Convert array

--- src/main/java/careercup/other/arrays/ConvertArray.java
+++ src/main/java/careercup/other/arrays/ConvertArray.java
@@ -15,9 +15,13 @@ public class ConvertArray {
   /**
    * Brute-force solution that takes O(n) space
    * and does not change input array.
+   * Time complexity: O(n)
    */
-  static String[] convert(String[] a, int n) {
-    int len = a.length, k;
+  static String[] convert(String[] a) {
+    if (a == null)
+      return null;
+    int len = a.length, n = len / 3, k;
+    // create new array
     String[] c = new String[len];
     for (int i = 0; i < n; i++) {
       k = 3*i;
@@ -26,5 +30,35 @@ public class ConvertArray {
       c[k+2] = a[2*n+i];
     }
     return c;
+  }
+
+  /**
+   * In-place solution
+   * Space complexity: O(1)
+   * Time complexity: O(n^1.3)
+   */
+  static void convertInPlace(String[] a) {
+    if (a == null)
+      return;
+    int n = a.length / 3;
+
+    for (int i = 0; i < a.length; i++) {
+      int swapIndex = getIndex(i, n);
+
+      while (swapIndex < i)
+        swapIndex = getIndex(swapIndex, n);
+
+      swap(a, i, swapIndex);
+    }
+  }
+
+  private static int getIndex(int currentIndex, int n) {
+    return (currentIndex % 3) * n + currentIndex/3;
+  }
+
+  private static void swap(String[] a, int i, int j) {
+    String tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
   }
 }
--- src/test/java/careercup/other/arrays/ConvertArrayTest.java
+++ src/test/java/careercup/other/arrays/ConvertArrayTest.java
@@ -18,9 +18,23 @@ public class ConvertArrayTest {
     String[] expected =
       {"a1", "b1", "c1", "a2", "b2", "c2", "a3", "b3", "c3", "a4", "b4", "c4"};
 
-    String[] converted = ConvertArray.convert(a, 4);
+    String[] converted = ConvertArray.convert(a);
     for (int i = 0; i < converted.length; i++) {
       assertEquals(expected[i] + "!=" + converted[i], expected[i], converted[i]);
+    }
+  }
+
+  @Test
+  public void testConvertInPlace() throws Exception {
+    String[] a =
+      {"a1", "a2", "a3", "a4", "b1", "b2", "b3", "b4", "c1", "c2", "c3", "c4"};
+
+    String[] expected =
+      {"a1", "b1", "c1", "a2", "b2", "c2", "a3", "b3", "c3", "a4", "b4", "c4"};
+
+    ConvertArray.convertInPlace(a);
+    for (int i = 0; i < a.length; i++) {
+      assertEquals(expected[i] + "!=" + a[i], expected[i], a[i]);
     }
   }
 }
