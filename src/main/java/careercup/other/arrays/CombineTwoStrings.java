4ef6bf84f23432864038be4565c2febfa33d5ed8
Artur Khalikov <a.khalikov@hh.ru>
2016-02-05 19:11:41 +0300

    Combine Two Strings

--- src/main/java/careercup/other/arrays/CombineTwoStrings.java
+++ src/main/java/careercup/other/arrays/CombineTwoStrings.java
@@ -6,7 +6,7 @@ package careercup.other.arrays;
  * of str1 and str2 in a way that maintains the left to right ordering
  * of the characters from each string.
  *
- * For example, given str1=”abc” and str2=”def”, str3=”dabecf” is a valid shuffle
+ * For example, given str1="abc" and str2="def", str3="dabecf" is a valid shuffle
  * since it preserves the character ordering of the two strings.
  * So, given these 3 strings write a function that detects
  * whether str3 is a valid shuffle of str1 and str2.
@@ -16,4 +16,36 @@ package careercup.other.arrays;
  * Created by akhalikov on 24/01/16
  */
 public class CombineTwoStrings {
+
+  static boolean isValidShuffle(String str1, String str2, String str3) {
+    validateInputArguments(str1, str2, str3);
+
+    // Basic validation that sum of lengths of str1 and str2 should be equal
+    // to the length of str3
+    if (str1.length() + str2.length() != str3.length())
+      return false;
+
+    int index1 = 0, index2 = 0;
+    for (int i = 0; i < str3.length(); i++) {
+      if (index1 < str1.length() && str3.charAt(i) == str1.charAt(index1))
+        index1++;
+      else if (index2 < str2.length() && str3.charAt(i) == str2.charAt(index2))
+        index2++;
+      else
+        return false;
+    }
+
+    return true;
+  }
+
+  private static void validateInputArguments(String str1, String str2, String str3) {
+    if (str1 == null)
+      throw new IllegalArgumentException("str1 is null");
+
+    if (str2 == null)
+      throw new IllegalArgumentException("str2 is null");
+
+    if (str3 == null)
+      throw new IllegalArgumentException("str3 is null");
+  }
 }
